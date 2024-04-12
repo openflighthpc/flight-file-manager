dc95a0128d9de31d6fa95ebb448c2251295bfaee
Cena Studio <mailforrgscid@gmail.com>
2024-04-12 17:10:00 +0100

    update unconfigured dashboard style

--- client/src/UnconfiguredDashboard.js
+++ client/src/UnconfiguredDashboard.js
@@ -1,28 +1,30 @@
 import React from 'react';
 
+import Blurb from "./Blurb";
+import {Footer} from "flight-webapp-components";
+
 function UnconfiguredDashboard() {
   return (
-    <div>
-      <p>
-        The Flight File Manager Service allows you to manage your files on
-        your cluster in a familiar file browser GUI (graphical user interface)
-        from within the comfort of your browser.
-      </p>
-
-      <p>
-        Before Flight File Manager Service can be used, it needs to be
-        configured by your system administrator.  It can be configured by
-        running:
-      </p>
-
-      <div className="card card-body">
-        <pre className="mb-0">
-          <code>
+    <>
+      <div
+        className="centernav col-8"
+      >
+        <div className='narrow-container'>
+          <Blurb />
+          <p>
+            Before Flight File Manager Service can be used, it needs to be
+            configured by your system administrator.  It can be configured by
+            running:
+          </p>
+          <p>
+            <code>
             flight service configure file-manager-webapp
-          </code>
-        </pre>
+            </code>
+          </p>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
